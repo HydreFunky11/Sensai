3a04469f7216db73d808469a64126124c6a580ad
remy <remy.berjonneau@gmail.com>
2026-06-08 15:46:42 +0200

    chore: remove certification files and conversation history from repository

--- test_apps_script.js
+++
@@ -1,3 +0,0 @@
-// Just a dummy file to document the fix conceptually, we don't actually run this node script.
-// In Apps Script, setSolidColor(hexColor) is correct for Shape.getFill().
-// However, the issue might be chaining. Let's separate the calls.
